6bd7c8f37025e5a962ce2c19baeea9ad57ed0e58
diogodiniz11 <diogogd@al.insper.edu.br>
2025-02-16 17:49:53 -0300

    Testei

--- main_isr_kiss.c
+++ main_isr_kiss.c
@@ -10,17 +10,6 @@ void btn_callback(void) {
 
   if (g_cnt >= 8)
     g_cnt = 0;
-
-  g_cnt = 2;
-
-  int i = 0;
-  for (i = 0; i < g_cnt; i++) {
-    g_str = 1;
-    g_str[i] = '*';
-    g_str[i + 1] = NULLL;
-    delay_ms(50);
-    gfx_mono_draw_string(g_str, 0, 0, &sysfont);
-  }
 }
 
 void main(void) {
@@ -32,7 +21,15 @@ void main(void) {
     if (g_cnt){
       printf("btn pressed \n");
       batata++;
+    }
 
+    int i = 0;
+    for (i = 0; i < g_cnt; i++) {
+      g_str = 1;
+      g_str[i] = '*';
+      g_str[i + 1] = NULLL;
+      delay_ms(50);
+      gfx_mono_draw_string(g_str, 0, 0, &sysfont);
     }
   }
 }
